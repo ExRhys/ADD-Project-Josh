9b1c6a375fe1966a3c3c91913feefd2d7c82162b
ExRhys <138288603+ExRhys@users.noreply.github.com>
2024-05-16 14:24:34 +0800

    Update Ardiuno_code.ino
    added sensor code from example code
    from: https://github.com/pololu/vl53l1x-arduino/blob/master/examples/ContinuousMultipleSensors/ContinuousMultipleSensors.ino

--- Ardiuno_code.ino
+++ Ardiuno_code.ino
@@ -1,9 +1,15 @@
 #include <Servo.h>
 #include <PID_v1.h>
 #include <Stepper.h>
+#include <Wire.h>
+#include <VL53L1X.h>
 
 Servo theta;  // create servo object to control a servo
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11) ;
+const uint8_t sensorCount = 2;
+const uint8_t xshutPins[sensorCount] = {4, 5};
+VL53L1X sensors[sensorCount];
+
 int crane_point = 0;
 int crane_contro[2][3] = { {90, 20, 15}, {180, 15, 10} }; //row is for option, then the 3 item array is rotation, in and out then height
 int crane_height= 0
@@ -17,6 +23,43 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 void crane_setup() {
   theta.attach(9);  // attaches the servo on pin 9 to the servo object  
   myStepper.setSpeed(400)
+
+  while (!Serial) {}
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(400000); // use 400 kHz I2C
+
+  // Disable/reset all sensors by driving their XSHUT pins low.
+  for (uint8_t i = 0; i < sensorCount; i++)
+  {
+    pinMode(xshutPins[i], OUTPUT);
+    digitalWrite(xshutPins[i], LOW);
+  }
+
+  // Enable, initialize, and start each sensor, one by one.
+  for (uint8_t i = 0; i < sensorCount; i++)
+  {
+    // Stop driving this sensor's XSHUT low. This should allow the carrier
+    // board to pull it high. (We do NOT want to drive XSHUT high since it is
+    // not level shifted.) Then wait a bit for the sensor to start up.
+    pinMode(xshutPins[i], INPUT);
+    delay(10);
+
+    sensors[i].setTimeout(500);
+    if (!sensors[i].init())
+    {
+      Serial.print("Failed to detect and initialize sensor ");
+      Serial.println(i);
+      while (1);
+    }
+
+    // Each sensor must have its address changed to a unique value other than
+    // the default of 0x29 (except for the last one, which could be left at
+    // the default). To make it simple, we'll just count up from 0x2A.
+    sensors[i].setAddress(0x2A + i);
+
+    sensors[i].startContinuous(50);  
+  }
 }
 
 void crane_theta(point) {
@@ -24,8 +67,7 @@ void crane_theta(point) {
 }
 
 int* distance_sensor() {
-  //read sensor values here
-  return {Sensor1,Sensor2}
+  return {sensors[0].read(),sensors[1].read()}
 }
 
 void crane_depth(point) {
